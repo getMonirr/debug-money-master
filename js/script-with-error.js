de7561bdc448dcfb3d48cb5da8b71814b1935137
Monir Hoshen <getmonirr@gmail.com>
2023-03-06 16:25:41 +0600

    error fixed

--- js/script-with-error.js
+++ js/script-with-error.js
@@ -1,8 +1,8 @@
 const calculateExpense = () => {
-  const income = document.querySelector("#income").value;
-  const food = document.querySelector("#food").value;
-  const rent = document.querySelector("#rent").value;
-  const clothes = document.querySelector("#clothes").value;
+  const income = document.querySelector("income").value;
+  const food = document.querySelector("food").value;
+  const rent = document.querySelector("rent").value;
+  const clothes = document.querySelectorAll("clothes").value;
 
   if (
     income < 0 ||
@@ -20,12 +20,11 @@ const calculateExpense = () => {
   // calculate expense
   const expense =
     parseInt(food) + parseInt(rent) + parseInt(clothes);
-    console.log(expense);
 
   // calculate balance
-  const balance = parseInt(income) - expense;
+  const balance = parseInt(income.value) - expense;
   //   validate income
-  if (expense > income) {
+  if (expense > income.value) {
     alert("Expenses cannot be more than income");
   } else {
     // view total expense and balance
@@ -35,15 +34,13 @@ const calculateExpense = () => {
 };
 
 const calculateSavings = () => {
-  const income = document.querySelector("#income").value;
   // calculate saving amount
   const savePercentage = document.getElementById("save").value;
 //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
-    return;
   }
-  const savingAmount = (savePercentage / 100) * income;
+  const savingAmount = (savePercentage / 100) + income;
 
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
